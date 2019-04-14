98b52e05315378a5f54f2b3220731b7a0cefd568
Alexandre THOMAS <athomas.nc@gmail.com>
2019-04-14 18:15:11 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# emblock-sdk-js: Emblock.co SDK for Javascript
+
+emblock-sdk-js is a sdk for the Emblock.co platform, it can be used on client and server side.
+
+Please check out our [website](https://emblock.co) before using our sdk.
+
+## Table of Contents
+
+- [Installing](#installing)
+- [API docs](#api-docs)
+- [Usage examples](#usage-examples)
+  - [Calling a constant function or get a state value](#calling-a-constant-function-or-get-a-state-value)
+  - [Calling a function](#calling-a-function)
+  - [Listening to events](#listening-to-events)
+- [Changelog](#changelog)
+- [Licence](#licence)
+
+## Installing
+
+```
+npm install emblock-sdk-js
+```
+
+## API docs
+
+See [`/doc/emblock-sdk-js.md`](./doc/emblock-sdk-js.md) for Node.js-like docs for the emblock-sdk-js classes.
+
+## Usage examples
+
+### Calling a constant function or get a state value
+
+```js
+const EmblockClient = require('emblock-sdk-js')
+const emblock = new EmblockClient('<API_KEY>', '<YOUR_CONTRACT_ID>')
+// { paramName1: "<param_value_1>", paramName2: "<param_value_2>" }
+const params = { owner: '0xcd92C45083aB059B1e5Af91c7cE58adf9D199e3c' }
+emblock
+  .callConstant('balanceOf', params)
+  .then(result => {
+    // result = [{"type": "uint256", "value": "100", "name": "balance"}]
+    // type = type of the result
+    // value = value of the result
+    // name = name of the result (can be null)
+    console.log('data=' + JSON.stringify(result))
+  })
+  .catch(err => {
+    console.log('error=' + err)
+  })
+```
+
+### Calling a function
+
+We are calling the transfer function of an ERC-20 smart contract.
+
+```js
+const EmblockClient = require('emblock-sdk-js')
+const emblock = new EmblockClient('<API_KEY>', '<YOUR_CONTRACT_ID>')
+const user1 = '0x73426F686Db8e511310a9fb90F9B22DB71ed53D4' // wallet address of the user 1
+var wallet = '0xcd92C45083aB059B1e5Af91c7cE58adf9D199e3c' // wallet address of the sender
+const params = { to: user1, value: '100' }
+client
+  .callFunction(wallet, 'transfer', params)
+  .then(resp => {
+    if (resp.isSuccessful) console.log('successful')
+    else console.log('failed')
+  })
+  .catch(err => {
+    console.log('error=' + err)
+  })
+```
+
+### Listening to Events
+
+Listening to events emitted by your smart contract.
+
+```js
+const EmblockClient = require('emblock-sdk-js')
+const emblock = new EmblockClient('<API_KEY>', '<YOUR_CONTRACT_ID>')
+
+emblock.addEventsListener(({ event, params }) => {
+  if (event === 'Transfer') {
+    console.log('this is a Transfer event !')
+  }
+})
+```
+
+## Changelog
+
+We're using the GitHub [releases][changelog] for changelog entries.
+
+## License
+
+[MIT](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,146 @@
+'use strict'
+
+import fetch from 'node-fetch'
+import WebSocket from 'isomorphic-ws'
+
+const SERVER_URL = 'http://localhost:9000'
+
+export default class EmblockClient {
+  constructor(apiKey, contractId) {
+    this.apiKey = apiKey
+    this.contractId = contractId
+    //this.events = new Map()
+  }
+
+  /**
+   * Call a constant function
+   * @param functionName the name of constant function to call
+   * @param params parameters to call the constant function
+   * @returns list of result
+   */
+  callConstant(functionName, params) {
+    const headers = generateHeaders(this.apiKey)
+    return fetch(`${SERVER_URL}/calls/${this.contractId}/${functionName}`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(params)
+    })
+      .then(parseJSON)
+      .then(resp => {
+        return new Promise(resolve => {
+          if (resp.ok) {
+            return resolve(resp.json)
+          }
+          var error = resp.json.error
+          reject({
+            status: resp.status,
+            error: error
+          })
+        })
+      })
+  }
+
+  /**
+   * Call a function
+   * @param {string} wallet pass the name of the wallet
+   * @param {string} functionName name of the function
+   * @param {object} params function parameters
+   */
+  callFunction(wallet, functionName, params) {
+    const headers = generateHeaders(this.apiKey)
+    if (wallet) headers['wallet'] = wallet
+    const path = `${SERVER_URL}/calls/${this.contractId}/${functionName}`
+    console.log('path =' + path)
+    return fetch(path, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(params)
+    })
+      .then(parseJSON)
+      .then(resp => {
+        //console.log('resp=' + JSON.stringify(resp))
+        return this.callFunctionStatus(resp.json.callId)
+          .then(parseJSON)
+          .then(response => {
+            return new Promise(resolve => {
+              //console.log('response=' + JSON.stringify(response.json))
+              resolve({
+                status: response.json.status,
+                isSuccessful: response.json.status === 'Successful'
+              })
+            })
+          })
+      })
+  }
+
+  callFunctionStatus(callId) {
+    const headers = {}
+    headers['content-type'] = 'application/json'
+    headers['Authorization'] = `Bearer ${this.apiKey}`
+    const path = `${SERVER_URL}/calls/${callId}/status`
+    console.log('path =' + path)
+    return fetch(path, {
+      method: 'GET',
+      headers: headers
+    }).then(this.parseJSON)
+  }
+
+  removeEventsListener() {
+    if (this.socket) {
+      this.socket.terminate()
+      this.socket = null
+    }
+  }
+
+  addEventsListener(cb) {
+    const wsUrl = SERVER_URL.replace('http', 'ws').replace('https', 'wss') + '/notifs'
+    // first time we call is - open the websocket
+    if (!this.socket) {
+      // create socket
+      this.socket = new WebSocket(wsUrl)
+
+      // on open -> subscribe to this contract events
+      this.socket.on('open', () => {
+        const data = {
+          type: 'contract_events',
+          data: {
+            contractId: this.contractId
+          }
+        }
+        const msg = JSON.stringify(data)
+        console.log('onOpen=' + msg)
+        this.socket.send(msg)
+      })
+
+      this.socket.on('message', message => {
+        const data = JSON.parse(message)
+        const eventName = data.name
+        if (cb) cb({ event: eventName, params: data.params })
+      })
+    }
+  }
+}
+
+const generateHeaders = function(apiKey) {
+  const headers = {}
+  headers['content-type'] = 'application/json'
+  headers['Authorization'] = `Bearer ${apiKey}`
+  return headers
+}
+
+const parseJSON = response => {
+  return new Promise(resolve => {
+    response
+      .text()
+      .then(text => {
+        return text ? JSON.parse(text) : {}
+      })
+      .then(json =>
+        resolve({
+          status: response.status,
+          ok: response.ok,
+          json
+        })
+      )
+  })
+}
